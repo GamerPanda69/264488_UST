b51cb605eec07a61043f6f9c86d131c377b19bb4
Akshay S A <84812051+GamerPanda69@users.noreply.github.com>
2025-04-04 16:08:54 +0530

    Update and rename __init__.py to hack1/__init__.py

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .strops import get_permutations, makeTitle, normalizeSpaces, getspan, reverseWords, removePunctuation, transform, countWords, characterMap
-__all__ = ["get_permutations", "makeTitle", "normalizeSpaces", "getspan", "reverseWords", "removePunctutation", "transform", "countWords", "characterMap"]
---
+++ hack1/__init__.py
@@ -0,0 +1,2 @@
+from .strops import get_permutations, makeTitle, normalizeSpaces, getspan, reverseWords, removePunctuation, transform, countWords, characterMap
+__all__ = ["get_permutations", "makeTitle", "normalizeSpaces", "getspan", "reverseWords", "removePunctutation", "transform", "countWords", "characterMap"]
